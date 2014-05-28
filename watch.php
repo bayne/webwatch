4a71975fee5165a950d2a2a583c1524633360d25
Brian Payne <bpayne@uci.edu>
2014-05-28 16:06:11 +0000

    Made more general by accepting arguments to control threshold

--- watch.php
+++ watch.php
@@ -4,10 +4,17 @@ use SebastianBergmann\Diff\Differ;
 
 $old = file_get_contents($argv[1]);
 $new = file_get_contents($argv[2]);
+if (isset($argv[3])) {
+    $threshold = $argv[3];
+} else {
+    $threshold = 3;
+}
 $differ = new Differ;
 $diff = $differ->diff($old,$new);
 $adds = preg_match_all('/^\\+.*$/m', $diff);
 $dels = preg_match_all('/^-.*$/m', $diff);
-if ($adds > 5 || $dels > 5) {
-    mail('bwpayne@gmail.com', 'Something has changed!', 'Something has changed');
+if ($adds > $threshold || $dels > $threshold) {
+    print "Something has changed! {$argv[2]}\n";
+    print $diff;
 }
+file_put_contents($argv[1], $new);
